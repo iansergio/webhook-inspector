acf7731ac9b7c043a20590602ee685bd5e2db9fa
Ian Sergio Helfenberger <iansergiohelf@gmail.com>
2025-11-19 15:49:18 -0300

    feat: main routes and database seed

--- api/src/server.ts
+++ api/src/server.ts
@@ -8,8 +8,11 @@ import {
 import { fastifySwagger} from '@fastify/swagger'
 import { fastifyCors } from '@fastify/cors'
 import ScalarApiReference from '@scalar/fastify-api-reference'
-import { listWebhooks } from './routes/get-list-webhooks'
 import { env } from './env'
+import { listWebhooks } from './routes/list-webhooks'
+import { getWebhook } from './routes/get-webhook'
+import { deleteWebhook } from './routes/delete-webhook'
+import { captureWebHook } from './routes/capture-webhook'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -38,6 +41,9 @@ app.register(ScalarApiReference, {
 })
 
 app.register(listWebhooks)
+app.register(getWebhook)
+app.register(deleteWebhook)
+app.register(captureWebHook)
 
 app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
 	console.log('🌊 HTTP server running on http://localhost:3333')
